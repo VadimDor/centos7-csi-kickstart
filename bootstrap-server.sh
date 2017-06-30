18c7339c5b1bc1f1658f08aa62d46e5c0cebb2aa
VadimDor <V.Dorez@mail.ru>
2017-06-30 20:58:08 +0300

    reset last 5 # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Changes to be committed: # # 	modified:   README.md # 	modified:   ask-config.sh # 	modified:   bootstrap-server.sh # 	modified:   hardened-centos7-kickstart # 	new file:   hardening-post

--- bootstrap-server.sh
+++ bootstrap-server.sh
@@ -1,14 +1,20 @@
 #!bin/sh
+
 # Check for root user
 if [[ $EUID -ne 0 ]]; then
-tput setaf 1;echo -e "\033[1mPlease re-run this script as root!\033[0m";tput sgr0
-exit 1
+ tput setaf 1;echo -e "\033[1mPlease re-run this script as root!\033[0m";tput sgr0
+ exit 1
+fi
+
 yum install -y wget
-# or wget goo.gl/MKqJsX -O  prepare-grub.sh
+
 wget  https://raw.githubusercontent.com/VadimDor/centos7-csi-kickstart/master/prepare-grub.sh -O  /boot/prepare-grub.sh
 chmod +x /boot/prepare-grub.sh
 wget  https://raw.githubusercontent.com/VadimDor/hardened-centos7-kickstart/master/config/hardening/hardened-centos.cfg -O  /boot/hardened-centos.cfg
 wget  https://raw.githubusercontent.com/VadimDor/centos7-csi-kickstart/master/ask-config.sh -O  /boot/ask-config.sh
 chmod +x /boot/ask-config.sh
-wget  https://raw.githubusercontent.com/VadimDor/hardened-centos7-kickstart/master/config/hardening/supplemental.cfg -O  /boot/supplemental.sh
+wget  https://raw.githubusercontent.com/VadimDor/hardened-centos7-kickstart/master/config/hardening/supplemental.sh -O  /boot/supplemental.sh
 chmod +x /boot/supplemental.sh
+wget  https://raw.githubusercontent.com/VadimDor/hardened-centos7-kickstart/master/config/hardening/fips-kernel-mode.sh -O  /root/hardening/fips-kernel-mode.sh
+chmod +x /boot/fips-kernel-mode.sh
+wget  https://raw.githubusercontent.com/VadimDor/hardened-centos7-kickstart/master/config/hardening/scap-security-guide-0.1.33-Linux-content.rpm -O  /root/hardening/scap-security-guide-0.1.33-Linux-content.rpm
